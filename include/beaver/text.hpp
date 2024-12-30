7b09bee7fc929a441c5eaaa4ca4553de8959fd4d
minhnn <minh.nn.00000@gmail.com>
2024-12-30 23:39:04 +0700

    add text shaded

--- include/beaver/text.hpp
+++ include/beaver/text.hpp
@@ -5,5 +5,9 @@
 namespace beaver
 {
 	sdl::texture make_text_solid(SDL_Renderer* rdr, const sdl::font& font, const std::string& content, const std::array<unsigned char, 4> color, int wraplength = 0);
+	sdl::texture make_text_shaded(SDL_Renderer* rdr, const sdl::font& font, const std::string& content,
+			const std::array<unsigned char, 4> fg,
+			const std::array<unsigned char, 4> bg,
+			int wraplength = 0);
 	sdl::texture make_text_blended(SDL_Renderer* rdr, const sdl::font& font, const std::string& content, const std::array<unsigned char, 4> color, int wraplength = 0);
 };
--- src/beaver/text.cpp
+++ src/beaver/text.cpp
@@ -14,6 +14,22 @@ sdl::texture beaver::make_text_solid(SDL_Renderer* rdr,
 	
 	return text;
 };
+sdl::texture beaver::make_text_shaded(SDL_Renderer* rdr,
+		const sdl::font& font, 
+		const std::string& content, 
+		const std::array<unsigned char, 4> fg,
+		const std::array<unsigned char, 4> bg,
+		int wraplength)
+{
+	SDL_Color fgc {fg[0], fg[1], fg[2], fg[3]};
+	SDL_Color bgc {bg[0], bg[1], bg[2], bg[3]};
+	SDL_Surface* temp = TTF_RenderUTF8_Shaded_Wrapped(font, content.c_str(), fgc, bgc, wraplength);
+	
+	sdl::texture text {SDL_CreateTextureFromSurface(rdr, temp)};
+	SDL_FreeSurface(temp);
+	
+	return text;
+};
 sdl::texture beaver::make_text_blended(SDL_Renderer* rdr,
 		const sdl::font& font, 
 		const std::string& content, 
