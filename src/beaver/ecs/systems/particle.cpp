447fdfadcab43b99f638d665c3e7420178b011c8
minh-nn-0 <nnminh.00000@gmail.com>
2026-02-04 10:05:36 +0700

    set manual emit particle to use correct config

--- src/beaver/ecs/systems/particle.cpp
+++ src/beaver/ecs/systems/particle.cpp
@@ -62,17 +62,17 @@ void particle_emitter::auto_emit()
 
 void particle_emitter::manual_emit(int number, float x, float y, const particle_config& config)
 {
-	float x0 = 0, x1 = _config._area.x, y0 = 0, y1 = _config._area.y;
+	float x0 = 0, x1 = config._area.x, y0 = 0, y1 = config._area.y;
 	if (x1 < x0) std::swap(x0,x1);
 	if (y1 < y0) std::swap(y0,y1);
 	for (int i = 0; i!= number; i++)
 	{
 		std::uniform_real_distribution<float> positionx_dist{x0,x1},
 											  positiony_dist{y0,y1},
-											  speed_dist{_config._speed_variation.x, _config._speed_variation.y},
-											  size_dist{_config._size_variation.x, _config._size_variation.y},
-											  spread_dist{- _config._spread / 2.f, _config._spread/ 2.f};
-		float spread_angle = spread_dist(_re) + _config._direction;
+											  speed_dist{config._speed_variation.x, config._speed_variation.y},
+											  size_dist{config._size_variation.x, config._size_variation.y},
+											  spread_dist{- config._spread / 2.f, config._spread/ 2.f};
+		float spread_angle = spread_dist(_re) + config._direction;
 
 
 		float size = size_dist(_re);
@@ -94,10 +94,10 @@ void particle_emitter::manual_emit(int number, float x, float y, const particle_
 		_particles.set_component<timing::countdown>(new_particle, lifetime);
 
 		std::array<unsigned char, 4> pcolor = {255,255,255,255};
-		if (_config._colors._value.size() > 0)
+		if (config._colors._value.size() > 0)
 		{
-			_particles.set_component<color_gradient>(new_particle, _config._colors);
-			pcolor = _config._colors._value.back()._color._value;
+			_particles.set_component<color_gradient>(new_particle, config._colors);
+			pcolor = config._colors._value.back()._color._value;
 		};
 		_particles.set_component<color>(new_particle, color{pcolor});
 	};
